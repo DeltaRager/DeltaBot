1ba17d91ab90f910ef0be172df978992201282cc
DeltaRager <abhinavtj@gmail.com>
2018-11-22 16:48:21 +0530

    Update Choose.py

--- Choose.py
+++ Choose.py
@@ -5,7 +5,7 @@ import asyncio
 
 async def choose(a):
     b,c,d=a.split(" ")
-        l = [c,d]
-        e = random.choice(l)
-        await garith.send_message(message.channel,'I choose *%s* ' % e)
+    l = [c,d]
+    e = random.choice(l)
+    await garith.send_message(message.channel,'I choose *%s* ' % e)
 
