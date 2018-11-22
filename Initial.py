ad0798249eede720923871f4fe3b0dd98140cc75
DeltaRager <abhinavtj@gmail.com>
2018-11-22 10:24:16 +0530

    Create Initial.py

---
+++ Initial.py
@@ -0,0 +1,38 @@
+import discord
+import asyncio
+
+garith = discord.Client()
+
+@garith.event
+async def on_ready():
+    print('System check completed')
+    print('Rebooting as')
+    print(garith.user.name)
+    print(garith.user.id)
+    print('------')
+
+
+@garith.event
+async def on_message(message):
+    if message.content.startswith('d!test'):
+        counter=0
+        tmp = await garith.send_message(message.channel, 'Calculating messages..')
+        async for log in garith.logs_from(message.channel,limit=100):
+            if log.author == message.autho:
+                counter +=1
+
+
+        await garith.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('d!sleep'):
+        userID = message.author.id
+        await asyncio.sleep(5)
+        await garith.send_message(message.channel, '<@%s> Die please!' % (userID))
+
+    elif message.content.startswith('d!night '):
+        await garith.send_message(message.channel, 'Good night <@%s>' % (message.mentions[0].id))
+
+garith.run(os.getenv('TOKEN'))
+        
+
+
+    
