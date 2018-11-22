e576c0de8a32a03a8991a6a1bb19a60832874350
DeltaRager <abhinavtj@gmail.com>
2018-11-22 21:40:02 +0530

    Update Initial.py

--- Initial.py
+++ Initial.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 garith = discord.Client()
-tod = commands.Bot(commands_prefix='g!')
+tod = commands.Bot(command_prefix = 'g!')
 
 
 
