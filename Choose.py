9c2a50cf2efddaaf52c407b60e948d0456035f31
DeltaRager <abhinavtj@gmail.com>
2018-11-22 18:14:09 +0530

    Update Choose.py

--- Choose.py
+++ Choose.py
@@ -2,7 +2,7 @@ import random
 import discord
 
 async def choose(a,garith,message):
-    x = a.split(",")
+    x = a.split(" ")
     del x[0]
     e = random.choice(x)
     await garith.send_message(message.channel,'I choose %s ' % e)
