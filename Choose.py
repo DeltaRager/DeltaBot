1612805d9e3c9db065ffb777c07d5455f5b3fcb3
DeltaRager <abhinavtj@gmail.com>
2018-11-22 17:01:51 +0530

    Update Choose.py

--- Choose.py
+++ Choose.py
@@ -5,9 +5,10 @@ import asyncio
 
 garith = discord.Client()
 
+@garith.event
 async def choose(a):
     b,c,d=a.split(" ")
     l = [c,d]
     e = random.choice(l)
-    await garith.send_message(message.channel,'I choose *%s* ' % e)
+    await garith.choose(message.channel,'I choose *%s* ' % e)
 
