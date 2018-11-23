5810e58411a3f7b4ba3f1f4ad4fcc3f357ee0e8c
DeltaRager <abhinavtj@gmail.com>
2018-11-23 20:33:21 +0530

    Update Voice.py

--- Voice.py
+++ Voice.py
@@ -13,7 +13,9 @@ async def leave(garith,message):
     voice_garith = garith.voice_garith_in(server)
     await voice_garith.disconnect()
 
-async def play(garith,message,url):
+async def play(garith,message):
+    a = message.content
+    b,url = a.split(" ")
     server = message.server
     voice_garith = garith.voice_client_in(server)
     player = await voice_garith.create_ytdl_player(url)
