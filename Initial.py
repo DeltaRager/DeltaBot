5889e3904421bc706443335c481937f4e9e214a3
DeltaRager <abhinavtj@gmail.com>
2018-11-23 22:12:15 +0530

    Update Initial.py

--- Initial.py
+++ Initial.py
@@ -6,9 +6,12 @@ import Choose
 import Help
 from discord.ext import commands
 from discord.ext.commands import Bot
+import json
+import os
 
 
 garith = discord.Client()
+os.chdir(r'E:\ForBot\DeltaBot')
 
 
 
@@ -48,6 +51,37 @@ async def on_message(message):
 
     elif message.content.startswith('g!help'):
         await Help.help(garith,message)
+
+@garith.event
+async def on_message(message):
+    if message.content.startswith('g!cchar'):
+        with open ('users.json', 'r') as f:
+            users = json.load(f)
+
+        await add_charname(users, message.author)
+        await add_charage(users, message.author)
+        await add_chargender(users, message.author)
+        await add_chardesc(users, message.author)
+        
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+async def add_charname(users, charname):
+    if not charname in users:
+        users[charname]['Name'] = {}
+
+async def add_charage(users, charage):
+    users[charage]['Age']={}
+
+async def add_chargender(users, chargender):
+    users[chargender]['Gender']={}
+
+async def add_chardesc(users, chardesc):
+    users[chardesc]['Description'] = {}
+          
+
+
+         
         
 
 garith.run(os.getenv('TOKEN'))
