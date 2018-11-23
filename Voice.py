d3ca55ffc032556e65a024efecaecb2078479332
DeltaRager <abhinavtj@gmail.com>
2018-11-23 18:23:15 +0530

    Update Voice.py

--- Voice.py
+++ Voice.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
-async def join(graith,message,ctx):
-    channel = ctx.message.author.voice.voice_channel
+async def join(garith,message):
+    channel = message.author.voice.voice_channel
     await garith.join_voice_channel(channel)
     
+async def leave(garith,mesage):
+    server = message.server
+    voice_garith = garith.voice_garith_in(server)
+    await voice_garith.disconnect()
