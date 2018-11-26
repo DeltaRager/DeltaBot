f280733ab26041a1b40335d576449e08bf017f1a
DeltaRager <abhinavtj@gmail.com>
2018-11-26 20:53:41 +0530

    Update query.py

--- query.py
+++ query.py
@@ -9,7 +9,7 @@ async def addquery(garith,message,time):
     question = message.content
     a,b=question.split("/")
     date = time.strftime("%H:%M")
-    await garith.say('Successfully submitted query!')
+    await garith..send_message(message.channel,'Successfully submitted query!')
     channel = discord.Object(id='516566401188888596')
     await garith.send_message(channel, '**The user:** %s **asked:** %s **at** %s' % (name,b,date))
     
