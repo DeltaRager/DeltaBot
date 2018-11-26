e3bd318deff5746f0feb3ecf2c0a38ee0b3e3b35
DeltaRager <abhinavtj@gmail.com>
2018-11-26 20:47:11 +0530

    Create query.py

---
+++ query.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import asyncio
+from datetime import date, datetime, timedelta
+import sys
+
+async def addquery(garith,message,time):
+    name = message.author.name
+    question = message.content
+    a,b=question.split("/")
+    date = time.strftime("%H:%M")
+    await client.say('Successfully submitted query!')
+    channel = discord.Object(id='516566401188888596')
+    await client.send_message(channel, '**The user:** %s **asked:** %s **at** %s' % (name,b,date))
+    
+    
+    
+    
