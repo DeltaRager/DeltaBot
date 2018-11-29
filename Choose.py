496674b3388dd5890512a183287a03475e4b9d6f
DeltaRager <abhinavtj@gmail.com>
2018-11-29 21:24:34 +0530

    Update Choose.py

--- Choose.py
+++ Choose.py
@@ -6,14 +6,14 @@ async def choose(a,garith,message):
     del x[0]
     a = [1,2,3]
     f = random.choice(a)
-    if (a==1):
+    if (f==1):
         e = random.choice(x)
         await garith.send_message(message.channel,'I choose **%s** ' % e)
-    elif (a==2):
+    elif (f==2):
         del x[1]
         e = random.choice(x)
         await garith.send_message(message.channel,'I choose **%s** ' % e)
-    elif (a==3):
+    elif (f==3):
         del x[0]
         e = random.choice(x)
         await garith.send_message(message.channel,'I choose **%s** ' % e)
