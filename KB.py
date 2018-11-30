bc5d84f0547c908932daff317f9f2ccf8af74e3e
DeltaRager <abhinavtj@gmail.com>
2018-11-30 17:50:06 +0530

    Update KB.py

--- KB.py
+++ KB.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 async def kick(garith,message):
-    a = message.mentions[0].id
+    a = message.mentions[0]
     b = message.content
     x = b.split('"')
     del x[0]
