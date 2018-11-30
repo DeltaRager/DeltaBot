3ee85459fcda6acc97cf17598844aa84e5a52d9d
DeltaRager <abhinavtj@gmail.com>
2018-11-30 19:11:39 +0530

    Update query.py

--- query.py
+++ query.py
@@ -4,13 +4,13 @@ import asyncio
 from datetime import date, datetime, timedelta
 import sys
 
-async def addquery(garith,message,time):
-    name = message.author.name
-    question = message.content
-    origin = message.channel
+async def addquery(ctx,garith,message,time):
+    name = ctx.message.author.name
+    question = ctx.message.content
+    origin = ctx.message.channel
     a,b=question.split("/")
     date = time.strftime("%H:%M")
-    await garith.send_message(message.channel,'Successfully submitted query!')
+    await garith.say('Successfully submitted query!')
     channel = discord.Object(id='516566401188888596')
     await garith.send_message(channel, '**The user:** %s **asked:** %s **from the channel:** %s' % (name,b,origin))
     
