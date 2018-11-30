71470b099e7dbfa933674ec3bee9942605c0e19b
DeltaRager <abhinavtj@gmail.com>
2018-11-30 18:24:53 +0530

    Update KB.py

--- KB.py
+++ KB.py
@@ -5,5 +5,5 @@ async def kick(garith,message):
     b = message.content
     x = b.split('"')
     del x[0]
-    
-    await garith.send_message(message.channel,"**The user:** %s **Was kicked by:** **Reason:** %s " % (message.mentions[0].mention,x))
+    a = get_member(message.mentions[0].id)
+    await garith.send_message(message.channel,"**The user:** %s **Was kicked by:** **Reason:** %s " % (a,x))
