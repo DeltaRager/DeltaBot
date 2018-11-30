710169f8238ac16465cebaab049e4b5bd08e57fb
DeltaRager <abhinavtj@gmail.com>
2018-11-30 18:37:18 +0530

    Update KB.py

--- KB.py
+++ KB.py
@@ -5,6 +5,7 @@ async def kick(garith,message):
     b = message.content
     x = b.split('"')
     del x[0]
+    k = discord.Server.get_member(user_id)
     j = message.mentions[0].id
-    k = discord.Server.get_member(j)
-    await garith.send_message(message.channel,"**The user:** %s **Was kicked by:** **Reason:** %s " % (k,x))
+    c = j.k
+    await garith.send_message(message.channel,"**The user:** %s **Was kicked by:** **Reason:** %s " % (c,x))
