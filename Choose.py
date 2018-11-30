69ce9c0f1cee252fcf1343b9ae791bd3f1c80d58
DeltaRager <abhinavtj@gmail.com>
2018-11-30 19:11:36 +0530

    Update Choose.py

--- Choose.py
+++ Choose.py
@@ -1,25 +1,26 @@
 import random
 import discord
+from discord.ext import commands
 
-async def choose(a,garith,message):
+async def choose(ctx,a,garith,message):
     x = a.split(" ")
     del x[0]
     a = [1,2,3]
     f = random.choice(a)
     if (f==1):
         e = random.choice(x)
-        await garith.send_message(message.channel,'I choose **%s** ' % e)
+        await garith.say('I choose **%s** ' % e)
     elif (f==2):
         del x[1]
         e = random.choice(x)
-        await garith.send_message(message.channel,'I choose **%s** ' % e)
+        await garith.say('I choose **%s** ' % e)
     elif (f==3):
         del x[0]
         e = random.choice(x)
-        await garith.send_message(message.channel,'I choose **%s** ' % e)
+        await garith.say('I choose **%s** ' % e)
 
 
-async def eightball(garith,message):
+async def eightball(ctx,garith,message):
     possible = [
         'Nope',
         'Not likely',
@@ -28,7 +29,7 @@ async def eightball(garith,message):
         'Definitely',
         'Maybe'
         ]
-    await garith.send_message(message.channel, random.choice(possible)) 
+    await garith.say(random.choice(possible)) 
         
     
     
