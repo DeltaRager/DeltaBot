d94351b6dcc24ba57c2e3e5e9bc56f5083618e69
DeltaRager <abhinavtj@gmail.com>
2018-12-05 20:38:32 +0530

    Update KB.py

--- KB.py
+++ KB.py
@@ -7,7 +7,7 @@ class admin:
         self.garith = garith
 
     @commands.command()
-    async def kick(ctx,message,garith,user: discord.Member):
+    async def kick(ctx,message,garith,  user: discord.Member):
         a = ctx.message.content
         b = a.split('-')
         del b[0]
