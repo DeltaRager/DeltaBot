836dbbfa0842b2c8d88b942717a6148fd99d4064
DeltaRager <abhinavtj@gmail.com>
2019-01-13 12:24:29 +0530

    Create subs.py

---
+++ subs.py
@@ -0,0 +1,10 @@
+import urllib.request
+import json
+import urllib
+import discord
+from discord.ext import commands
+
+async def subcount(ctx,garith):
+    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCGZPDIh0EIn4Rn8jdf6gSDA&key=AIzaSyC9PM7mQBumbwCFo18sBq9-NdZf5EJgDy4").read()
+    subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+    await garith.say("*We have * **"+ "{:,d}".format(int(subs)) + "** *Subscribers*")
