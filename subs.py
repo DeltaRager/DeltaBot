d56c2407ceeb4e153e8de165ac3f71f96333de15
DeltaRager <abhinavtj@gmail.com>
2019-01-13 12:27:25 +0530

    Update subs.py

--- subs.py
+++ subs.py
@@ -7,4 +7,4 @@ from discord.ext import commands
 async def subcount(ctx,garith):
     data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCGZPDIh0EIn4Rn8jdf6gSDA&key=AIzaSyC9PM7mQBumbwCFo18sBq9-NdZf5EJgDy4").read()
     subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
-    await garith.say("*We have * **"+ "{:,d}".format(int(subs)) + "** *Subscribers*")
+    await garith.say("*We have* **"+ "{:,d}".format(int(subs)) + "** *Subscribers*")
