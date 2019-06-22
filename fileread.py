c771d0c2903f8bde2e0c2c0c167449183073805b
DeltaRager <abhinavtj@gmail.com>
2019-06-22 20:04:20 +0530

    Update fileread.py

--- fileread.py
+++ fileread.py
@@ -4,4 +4,6 @@ from discord.ext import commands
 
 async def text(ctx):
         f= open("itworks.txt","w+")
+        for i in range(10):
+                f.write("This is line %d\r\n" % (i+1))
         f.close()
