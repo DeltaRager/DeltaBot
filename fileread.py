d94a541b6929566b2def611a6cf67d5fc472da13
DeltaRager <abhinavtj@gmail.com>
2019-06-22 20:13:31 +0530

    Update fileread.py

--- fileread.py
+++ fileread.py
@@ -4,6 +4,22 @@ import json
 
 
 async def text(ctx):
-        data='it works'
-        with open('data.json', 'w') as outfile:
+        data = {}
+        data['people'] = []
+        data['people'].append({
+                'name': 'hello',
+                'website': 'stackabuse.com',
+                'from': 'Nebraska'
+                })
+        data['people'].append({
+                'name': 'Larry',
+                'website': 'google.com',
+                'from': 'Michigan'
+                })
+        data['people'].append({
+                'name': 'Tim',
+                'website': 'apple.com',
+                'from': 'Alabama'
+                })
+        with open('data.txt', 'w') as outfile:
                 json.dump(data, outfile)
